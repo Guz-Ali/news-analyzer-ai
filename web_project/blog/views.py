2c6452b7b5dc989e09fb013bc9445ec6f54089ef
Ali Guzelyel <aguzelyel@hawk.iit.edu>
2024-07-13 12:15:07 -0500

    implemented templates and bootstrap

--- web_project/blog/views.py
+++ web_project/blog/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        "author": "Ali",
+        "title": "blog post 1",
+        "content": "first post content",
+        "date_posted": "August 1, 2024",
+    },
+    {
+        "author": "Veli",
+        "title": "blog post 2",
+        "content": "seoncd post content",
+        "date_posted": "August 2, 2024",
+    },
+    {
+        "author": "Deli",
+        "title": "blog post 3",
+        "content": "third post content",
+        "date_posted": "August 3, 2024",
+    },
+]
 
 
 def home(request):
-    return HttpResponse("<h1>Blog Home </h1>")
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About </h1>")
+    return render(request, "blog/about.html", {"title": "About"})
