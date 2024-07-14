1283c0daf45ded6ac8703ab3637f1c2921080cb3
Ali Guzelyel <aguzelyel@hawk.iit.edu>
2024-07-14 06:29:52 -0500

    redacted views dummy data

--- web_project/blog/views.py
+++ web_project/blog/views.py
@@ -1,27 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        "author": "Ali",
-        "title": "blog post 1",
-        "content": "first post content",
-        "date_posted": "August 1, 2024",
-    },
-    {
-        "author": "Veli",
-        "title": "blog post 2",
-        "content": "seoncd post content",
-        "date_posted": "August 2, 2024",
-    },
-    {
-        "author": "Deli",
-        "title": "blog post 3",
-        "content": "third post content",
-        "date_posted": "August 3, 2024",
-    },
-]
-
 
 def home(request):
     context = {"posts": Post.objects.all()}
