a76fe0dcb913524dbe52c2f6e582fd47e069c66a
Ali Guzelyel <aguzelyel@hawk.iit.edu>
2024-07-15 09:01:51 -0500

    profile picture and auto-create

--- web_project/web_project/urls.py
+++ web_project/web_project/urls.py
@@ -19,6 +19,9 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from user import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -36,3 +39,5 @@ urlpatterns = [
     ),
     path("", include("blog.urls")),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
