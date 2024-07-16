c7428cfa9f125cce36418d3931d08404aa9ee657
Ali Guzelyel <aguzelyel@hawk.iit.edu>
2024-07-16 15:22:09 -0500

    emailing system for password reset

--- web_project/web_project/urls.py
+++ web_project/web_project/urls.py
@@ -37,7 +37,34 @@ urlpatterns = [
         auth_views.LogoutView.as_view(template_name="user/logout.html"),
         name="logout",
     ),
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(template_name="user/password_reset.html"),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="user/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="user/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset-complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="user/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
     path("", include("blog.urls")),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
