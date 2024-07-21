97dadde2816cb0e4433f6dc8adf5443648482632
Guz-Ali <aliguzelyel@gmail.com>
2024-07-21 05:40:21 +0000

    added ssl certificate

--- web_project/web_project/settings.py
+++ web_project/web_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [config["IP_ADDRESS"]]
+ALLOWED_HOSTS = ["www.aliguzelyel.com", config["IP_ADDRESS"]]
 
 
 # Application definition
