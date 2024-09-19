9ea55295fe271cc9eb079d63043091f8b6c37201
Ali Guzelyel <aguzelyel@hawk.iit.edu>
2024-09-18 20:41:06 -0500

    restructured AINewsGenerator

--- web_project/blog/tasks.py
+++ web_project/blog/tasks.py
@@ -6,26 +6,22 @@ import json
 import google.generativeai as genai
 from newsapi import NewsApiClient
 
-class AINewsGenerator:
-    def __init__(self):
-        self._get_creds()
-        self._set_client()
-        self._set_model()
 
-    def _get_creds(self):
+class NewsClient:
+    def __init__(self):
+        self._get_client_creds()
+        self._set_client()
+        super().__init__()
+
+    def _get_client_creds(self):
         with open("/etc/config.json") as config_file:
             config = json.load(config_file)
-        self.creds = {}
-        self.creds["GEMINI_API_KEY"] = config["GEMINI_API_KEY"]
-        self.creds["NEWS_API_KEY"] = config["NEWS_API_KEY"]
-        self.creds["PROMPT"] = config["PROMPT"]
+        if not hasattr(self, "client_creds"):
+            self.client_creds = {}
+        self.client_creds["NEWS_API_KEY"] = config["NEWS_API_KEY"]
 
     def _set_client(self):
-        self.client = NewsApiClient(api_key=self.creds["NEWS_API_KEY"])
-    
-    def _set_model(self):
-        genai.configure(api_key=self.creds["GEMINI_API_KEY"])
-        self.model = genai.GenerativeModel("gemini-1.5-flash")
+        self.client = NewsApiClient(api_key=self.client_creds["NEWS_API_KEY"])
 
     def get_sources(self, lang="en"):
         try:
@@ -40,9 +36,9 @@ class AINewsGenerator:
             return sources
         except ValueError as e:
             exit(e, "API call (get_sources) status NOT OK")
-    
+
     def get_articles(self, sources):
-        try:    
+        try:
             top_headlines = self.client.get_top_headlines(sources=sources)
             if top_headlines["status"] != "ok":
                 raise ValueError
@@ -50,7 +46,31 @@ class AINewsGenerator:
             return articles
         except ValueError as e:
             exit(e, "API call (get_articles) status NOT OK")
-    
+
+
+class AIModel:
+    def __init__(self):
+        self._get_model_creds()
+        self._set_model()
+        super().__init__()
+
+    def _get_model_creds(self):
+        with open("/etc/config.json") as config_file:
+            config = json.load(config_file)
+        if not hasattr(self, "model_creds"):
+            self.model_creds = {}
+        self.model_creds["GEMINI_API_KEY"] = config["GEMINI_API_KEY"]
+        self.model_creds["PROMPT"] = config["PROMPT"]
+
+    def _set_model(self):
+        genai.configure(api_key=self.model_creds["GEMINI_API_KEY"])
+        self.model = genai.GenerativeModel("gemini-1.5-flash")
+
+
+class AINewsGenerator(NewsClient, AIModel):
+    def __init__(self):
+        super().__init__()
+
     def prepare_prompt(self, articles):
         all_news = []
         for article in articles:
@@ -79,9 +99,9 @@ class AINewsGenerator:
             news_string = "".join(news)
             all_news.append(news_string)
 
-        all_news.append(self.creds["PROMPT"])
+        all_news.append(self.model_creds["PROMPT"])
         return all_news
-    
+
     def collect_news(self):
         sources = self.get_sources()
         articles = self.get_articles(sources)
@@ -93,9 +113,10 @@ class AINewsGenerator:
         return response.text
 
     def create_ai_post(self, ai_news_text):
-        ai_post = Post(title="Today's News",
-                            content=ai_news_text,
-                            date_posted=timezone.now(),
-                            author = get_object_or_404(User, username="AI_NEWS"))
+        ai_post = Post(
+            title="Today's News",
+            content=ai_news_text,
+            date_posted=timezone.now(),
+            author=get_object_or_404(User, username="AI_NEWS"),
+        )
         ai_post.save()
-
