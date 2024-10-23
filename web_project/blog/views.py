1860d6dd971657d404af13bf0e00c4c9b3d11663
Enes Demirel <enesdemirelus@gmail.com>
2024-10-23 12:35:07 -0500

    added a image selector inside new post and update post

--- web_project/blog/views.py
+++ web_project/blog/views.py
@@ -41,7 +41,7 @@ class PostDetailView(DetailView):
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ["title", "content"]
+    fields = ["title", "content", "image"]
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -50,7 +50,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    fields = ["title", "content"]
+    fields = ["title", "content", "image"]
 
     def form_valid(self, form):
         form.instance.author = self.request.user
