17b2805e217d227f3eeebe6ee46980528f8010f8
Enes Demirel <enesdemirelus@gmail.com>
2024-10-28 11:08:50 -0500

    model created, added to admins and migrated

--- web_project/blog/admin.py
+++ web_project/blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Post
+from .models import NewsCard
 
 
 admin.site.register(Post)
+admin.site.register(NewsCard)
--- web_project/blog/models.py
+++ web_project/blog/models.py
@@ -14,3 +14,19 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("post-detail", kwargs={"pk": self.pk})
+
+class NewsCard(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now) 
+    date_created = models.DateTimeField(auto_now_add=True)
+    url_of_the_news = models.URLField()
+    summary = models.TextField(max_length=100) #can be changed later.
+    image = models.ImageField(upload_to='ai_images/', null= True, blank= True, default="ai_default.jpg")
+    is_public = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
